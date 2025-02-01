b1e43e2bd095539cb868cb63f5c644b0c954f497
Jorge Fernando Álava <jorgefernando.alava@seidor.com>
2025-02-01 01:18:43 +0000

    Revert "updated astro config, added react compiler"
    This reverts commit 5cd9ca949d4cf2d16f4b5fd6573df2c416f00840.

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -4,45 +4,17 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
-import sitemap from "astro-sitemap";
-import robotsTxt from "astro-robots-txt";
-import react from "@astrojs/react";
-const compilerConfig = {
-  target: "19",
-};
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  site: "https://shortener.jfa.ovh",
   adapter: cloudflare({
     imageService: "passthrough",
   }),
-
   vite: {
     plugins: [tailwindcss()],
     ssr: {
       external: ["path"].map((i) => `node:${i}`),
     },
   },
-
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      host: "shortener.jfa.ovh",
-      sitemap: ["https://shortener.jfa.ovh/sitemap-index.xml"],
-      policy: [
-        {
-          userAgent: "*",
-          allow: "/",
-          crawlDelay: 10,
-        },
-      ],
-    }),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", compilerConfig]],
-      },
-    }),
-  ],
 });
