c58c1084c4f00279c32c3fa4398274b9de82b431
Jorge Fernando Álava <git@jfa.dev>
2025-02-02 16:44:38 +0100

    mdx ssr?

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -8,6 +8,8 @@ import robotsTxt from "astro-robots-txt";
 
 import sitemap from "astro-sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
@@ -23,18 +25,15 @@ export default defineConfig({
     },
   },
 
-  integrations: [
-    robotsTxt({
-      host: "shortener.jfa.ovh",
-      sitemap: ["https://shortener.jfa.ovh/sitemap-index.xml"],
-      policy: [
-        {
-          userAgent: "*",
-          allow: "/",
-          crawlDelay: 10,
-        },
-      ],
-    }),
-    sitemap(),
-  ],
+  integrations: [robotsTxt({
+    host: "shortener.jfa.ovh",
+    sitemap: ["https://shortener.jfa.ovh/sitemap-index.xml"],
+    policy: [
+      {
+        userAgent: "*",
+        allow: "/",
+        crawlDelay: 10,
+      },
+    ],
+  }), sitemap(), mdx()],
 });
