8509ca19d2a821d7be63d509b0ecbcf0b31fa7cf
Jorge Fernando Álava <git@jfa.dev>
2025-02-02 15:17:01 +0100

    toast implementation

---
+++ www/src/i18n/components/toasts/BadInput.i18n.ts
@@ -0,0 +1,6 @@
+import type { RequiredLocales } from "@/utils/get-locale";
+
+export const localizedBadInput: RequiredLocales = {
+  es: "Algo ha ido mal, revisa el enlace y prueba de nuevo",
+  en: "Something went wrong, check the link and try again",
+};
