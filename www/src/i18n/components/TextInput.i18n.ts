41b0ed049593cbc8118b354d49158071b7ac6878
Jorge Fernando Álava <git@jfa.dev>
2025-02-03 22:46:06 +0100

    updated placeholder

--- www/src/i18n/components/TextInput.i18n.ts
+++ www/src/i18n/components/TextInput.i18n.ts
@@ -1,8 +1,8 @@
 import type { RequiredLocales } from "@/utils/get-locale";
 
 export const localizedTextInputPlaceholder: RequiredLocales = {
-  es: "Tu enlace aquí...",
-  en: "Your link here...",
+  es: "https://... tu enlace aquí",
+  en: "https://... your link here",
 };
 
 export const localizedShortenLinkButton: RequiredLocales = {
