c98f02e22b3b4cf2662320950bc289b8e2c75544
Jorge Fernando Álava <jorgefernando.alava@seidor.com>
2025-02-04 09:56:05 +0000

    refactored some shortened link logic

--- www/src/i18n/meta/MetaTags.i18n.ts
+++ www/src/i18n/meta/MetaTags.i18n.ts
@@ -1,11 +1,11 @@
 import type { RequiredLocales } from "@/utils/get-locale";
 
 export const localizedMetaDescription: RequiredLocales = {
-  es: "Link shortening service, by Jorge Fernando Álava",
+  es: "Link shortener service, by Jorge Fernando Álava",
   en: "Servicio de acortamiento de enlaces, por Jorge Fernando Álava",
 };
 
 export const localizedMetaKeywords: RequiredLocales = {
-  es: "link shortener, link shortening, jorge fernando alava, jfalava, jorgefalava",
+  es: "link shortener, link shortener, jorge fernando alava, jfalava, jorgefalava",
   en: "acortador de enlaces, acortamiento de enlaces, jorge fernando alava, jfalava, jorgefalava",
 };
