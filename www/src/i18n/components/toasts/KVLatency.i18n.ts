ed1d1201fa06d6304e9b9fe89db93d4022c45c07
Jorge Fernando Álava <jorgefernando.alava@seidor.com>
2025-02-04 10:28:59 +0000

    prettify toasts

--- www/src/i18n/components/toasts/KVLatency.i18n.ts
+++
@@ -1,6 +0,0 @@
-import type { RequiredLocales } from "@/utils/get-locale";
-
-export const localizedKVLatency: RequiredLocales = {
-  es: "El enlace puede tardar hasta 60s en estar preparado",
-  en: "The link may take up to 60s to be ready",
-};
