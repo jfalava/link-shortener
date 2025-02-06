a2201aa9eac1180edcb7f72d06d34d9080648115
Jorge Fernando Álava <git@jfa.dev>
2025-02-06 12:33:53 +0100

    i18n tweak

--- www/src/i18n/components/LinkHistory.i18n.ts
+++ www/src/i18n/components/LinkHistory.i18n.ts
@@ -1,8 +1,8 @@
 import type { RequiredLocales } from "@/utils/get-locale";
 
 export const localizedLinkHistory: RequiredLocales = {
-  es: "Historial de enlaces acortados",
-  en: "Shortened links history",
+  es: "Historial",
+  en: "History",
 };
 
 export const localizedDeleteHistory: RequiredLocales = {
