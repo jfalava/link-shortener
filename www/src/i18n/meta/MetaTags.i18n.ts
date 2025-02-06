0f8343b89405c3491ff0df6c7ec76005e8b8934a
Jorge Fernando Álava <git@jfa.dev>
2025-02-06 13:09:12 +0100

    mixed keys

--- www/src/i18n/meta/MetaTags.i18n.ts
+++ www/src/i18n/meta/MetaTags.i18n.ts
@@ -1,11 +1,11 @@
 import type { RequiredLocales } from "@/utils/get-locale";
 
 export const localizedMetaDescription: RequiredLocales = {
-  es: "Link shortener service, by Jorge Fernando Álava",
-  en: "Servicio de acortamiento de enlaces, por Jorge Fernando Álava",
+  en: "Link shortener service, by Jorge Fernando Álava",
+  es: "Servicio de acortamiento de enlaces, por Jorge Fernando Álava",
 };
 
 export const localizedMetaKeywords: RequiredLocales = {
-  es: "link shortener, link shortener, jorge fernando alava, jfalava, jorgefalava",
-  en: "acortador de enlaces, acortamiento de enlaces, jorge fernando alava, jfalava, jorgefalava",
+  en: "link shortener, link shortener, jorge fernando alava, jfalava, jorgefalava",
+  es: "acortador de enlaces, acortamiento de enlaces, jorge fernando alava, jfalava, jorgefalava",
 };
