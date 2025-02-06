3acd70c445126fb6256f00b7c6075ec024efadd9
Jorge Fernando Álava <git@jfa.dev>
2025-02-06 10:27:10 +0100

    added localization

--- www/src/i18n/components/LinkHistory.i18n.ts
+++ www/src/i18n/components/LinkHistory.i18n.ts
@@ -4,3 +4,8 @@ export const localizedLinkHistory: RequiredLocales = {
   es: "Historial de enlaces acortados",
   en: "Shortened links history",
 };
+
+export const localizedDeleteHistory: RequiredLocales = {
+  es: "Borra esta entrada del historial",
+  en: "Delete this entry from your history",
+};
