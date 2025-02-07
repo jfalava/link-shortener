b02031b6eb2f6cda4f15efb311398237d78f4e56
Jorge Fernando Álava <git@jfa.dev>
2025-02-07 22:54:34 +0100

    disable ctrl+k browser bind

--- www/src/utils/input-focus.ts
+++ www/src/utils/input-focus.ts
@@ -1,6 +1,9 @@
 export function inputFocus(inputId: string): void {
   document.addEventListener("keydown", (event) => {
     if (event.ctrlKey && event.key === "k") {
+      // disable browser ctrl+k bind
+      event.preventDefault();
+
       const inputElement = document.getElementById(inputId);
       if (inputElement) {
         (inputElement as HTMLInputElement).focus();
