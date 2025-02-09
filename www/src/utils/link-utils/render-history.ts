93f8df8e250d2970dfc814391caa4184bba3267f
Jorge Fernando Álava <git@jfa.dev>
2025-02-09 20:57:20 +0100

    render history tweaks

--- www/src/utils/link-utils/render-history.ts
+++ www/src/utils/link-utils/render-history.ts
@@ -34,7 +34,7 @@ export function renderHistory(locale: keyof Locale) {
         <div class="flex justify-between items-center">
           <div class="flex-1 overflow-hidden">
             <div class="flex items-center pb-1">
-              <div class="pr-1 select-none cursor-pointer copy-button" data-string="${entry.shortUrl}" title="${localizedClickToCopy[locale]}">
+              <div class="pr-2 select-none cursor-pointer copy-button" data-string="${entry.shortUrl}" title="${localizedClickToCopy[locale]}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></g></svg>
               </div>
               <a href="${entry.shortUrl}" target="_blank" class="text-blue-500 no-underline block hover:underline">
@@ -43,8 +43,8 @@ export function renderHistory(locale: keyof Locale) {
             </div>
             <div>
               <div class="flex items-center">
-                <div class="pr-1 select-none">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="18" cy="18" r="3"/><circle cx="6" cy="6" r="3"/><path d="M6 21V9a9 9 0 0 0 9 9"/></g></svg>
+                <div class="pr-2 select-none">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="18" cy="18" r="3"/><circle cx="6" cy="6" r="3"/><path d="M6 21V9a9 9 0 0 0 9 9"/></g></svg>
                 </div>
                 <div class="text-sm overflow-hidden text-ellipsis whitespace-nowrap select-none text-mono">
                   ${entry.originalUrl}
@@ -68,6 +68,7 @@ export function renderHistory(locale: keyof Locale) {
         </div>
       </div>
     `,
+        9,
       )
       .join("");
 
